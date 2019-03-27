3311ef6e7c19e3685c6202c1dcd966dd70bee648
sk521 <samkwon521@gmail.com>
2019-03-27 14:35:14 -0400

    217 contains duplicates

---
+++ easy/217_contains_duplicates.js
@@ -0,0 +1,25 @@
+/*
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+*/
+
+function containsDuplicates(arr) {
+  let hash = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (hash[arr[i]] === undefined) {
+      hash[arr[i]] = 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(containsDuplicates([1, 2, 3, 1]));
