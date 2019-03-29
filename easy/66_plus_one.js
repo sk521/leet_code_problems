4c86bd78f4d8d92332fa96cd12e249a8051ae1f7
sk521 <samkwon521@gmail.com>
2019-03-29 12:15:54 -0400

    66 completed

--- easy/66_plus_one.js
+++ easy/66_plus_one.js
@@ -18,5 +18,12 @@ Explanation: The array represents the integer 4321.
 */
 
 function plusOne(digits) {
-
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (++digits[i] > 9) digits[i] = 0;
+    else return digits;
+  }
+  digits.unshift(1);
+  return digits;
 }
+
+console.log(plusOne([1,2,3]));
