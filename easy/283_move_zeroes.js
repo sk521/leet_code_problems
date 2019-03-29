961235d59046e5217c147420c39f330eefccdb0a
sk521 <samkwon521@gmail.com>
2019-03-29 16:08:12 -0400

    238 move zeroes complete

--- easy/283_move_zeroes.js
+++ easy/283_move_zeroes.js
@@ -13,3 +13,25 @@ Minimize the total number of operations.
 
 
 
+function moveZeroes(nums) {
+  let i = 0;
+  let j = 0;
+
+  while(j < nums.length) {
+    if (nums[j] === 0) {
+      j++;
+    } else {
+      nums[i] = nums[j];
+      i++;
+      j++;
+    }
+  }
+
+  while (i < nums.length) {
+    nums[i++] = 0;
+  }
+
+  return nums;
+}
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
