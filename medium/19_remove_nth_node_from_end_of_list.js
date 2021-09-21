8d490b5274519ab0b147727449412f90a7be2226
sk521 <samkwon521@gmail.com>
2021-09-21 16:45:16 -0400

    19 remove nth node from end of list

---
+++ medium/19_remove_nth_node_from_end_of_list.js
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+*/
+
+
+const removeNthFromEnd = (head, n) => {
+  let fast = head;
+  let slow = head;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  if (!fast) return head.next;
+  while(fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  slow.next = slow.next.next;
+  return head;
+}
